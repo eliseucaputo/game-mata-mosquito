039d6882e2fd4ccdd008ea33b54042bd16d273ac
Eliseu Caputo <eliseujunior188@gmail.com>
2023-01-21 10:10:22 -0300

    posicao randomica

--- game/jogo.js
+++ game/jogo.js
@@ -11,7 +11,59 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
-let posicaoX = Math.floor(Math.random() * largura) 
-let posicaoY = Math.floor(Math.random() * altura) 
 
-console.log(posicaoX, posicaoY)
+function posicaoRandomica() {
+
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
+
+    let posicaoX = Math.floor(Math.random() * largura) - 90
+    let posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criar o elemento html
+
+    let mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
+    document.body.appendChild(mosquito)
+    
+}
+
+function tamanhoAleatorio() {
+    let classe = Math.floor(Math.random() * 3)
+    
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1: 
+            return 'mosquito2'
+        
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    let classe = Math.floor(Math.random() * 2)
+    
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+
+        case 1: 
+            return 'ladoB'
+    }
+}
+
